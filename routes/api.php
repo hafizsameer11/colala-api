048771b6658c68056406c5363a3d11af360ccc26
hafiz sameer <hmstech11@gmail.com>
2025-09-17 14:34:01 +0500

    hel

--- routes/api.php
+++ routes/api.php
@@ -2,8 +2,30 @@
 
 use App\Http\Controllers\Api\AuthController;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Route;
 
+Route::get('/optimize-app', function () {
+    Artisan::call('optimize:clear'); // Clears cache, config, route, and view caches
+    Artisan::call('cache:clear');    // Clears application cache
+    Artisan::call('config:clear');   // Clears configuration cache
+    Artisan::call('route:clear');    // Clears route cache
+    Artisan::call('view:clear');     // Clears compiled Blade views
+    Artisan::call('config:cache');   // Rebuilds configuration cache
+    Artisan::call('route:cache');    // Rebuilds route cache
+    Artisan::call('view:cache');     // Precompiles Blade templates
+    Artisan::call('optimize');       // Optimizes class loading
+
+    return "Application optimized and caches cleared successfully!";
+});
+Route::get('/migrate', function () {
+    Artisan::call('migrate');
+    return response()->json(['message' => 'Migration successful'], 200);
+});
+// Route::get('/migrate/rollback', function () {
+//     Artisan::call('migrate:rollback');
+//     return response()->json(['message' => 'Migration rollback successfully'], 200);
+// });
 
 Route::get('/un-auth',function(){
     return response()->json(['message' => 'Unauthenticated.'], 401);
