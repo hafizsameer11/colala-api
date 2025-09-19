f4388b3c9e2ba2f6970a2dd1877d5bd69f5ed73d
hafiz sameer <hmstech11@gmail.com>
2025-09-19 16:13:52 +0500

    jel

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -3,13 +3,26 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Storage;
 
 class Category extends Model
 {
-    protected $fillable=[
+    protected $fillable = [
         'title',
         'image',
         'color'
     ];
 
+    // Make sure the custom attribute is added automatically in response
+    protected $appends = ['image_url'];
+
+    /**
+     * Accessor for full image URL
+     */
+    public function getImageUrlAttribute()
+    {
+        return $this->image 
+            ? Storage::url($this->image)  // Generates "/storage/..." URL
+            : null;
+    }
 }
