2a3d819870ce38b0e47f09ac4d0e2c3145a08a9e
hafiz sameer <hmstech11@gmail.com>
2025-09-26 03:04:13 +0500

    hel

---
+++ app/Http/Controllers/Api/StoreController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Helpers\ResponseHelper;
+use App\Http\Controllers\Controller;
+use App\Models\Store;
+use Illuminate\Http\Request;
+
+class StoreController extends Controller
+{
+    public function getAll(Request $req) {
+        // This is a placeholder implementation. Replace with actual logic to fetch stores.
+        try{
+            $Stores=Store::latest()->get();
+            return ResponseHelper::success($Stores);
+        }catch(\Exception $e){
+            return ResponseHelper::error( $e->getMessage(), 500);
+        }
+    }
+    public function getById(Request $req, $storeId) {
+        try{
+            $store=Store::with('user','socialLinks','businessDetails','addresses','deliveryPricing','categories')->findOrFail($storeId);
+            return ResponseHelper::success($store);
+        }catch(\Exception $e){
+            return ResponseHelper::error( $e->getMessage(), 500);
+        }
+    }
+}
