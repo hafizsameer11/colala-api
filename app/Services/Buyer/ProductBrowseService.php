aeeab022df22c503092da84b68d39d5402f47c7a
hafiz sameer <hmstech11@gmail.com>
2025-09-25 15:01:49 +0500

    hel{

--- app/Services/Buyer/ProductBrowseService.php
+++ app/Services/Buyer/ProductBrowseService.php
@@ -8,15 +8,9 @@ use App\Models\Product;
 
 class ProductBrowseService {
     public function byCategory(int $categoryId) {
-        $category = Category::with('children:id,parent_id')->findOrFail($categoryId);
+       return Product::where('category_id',$categoryId)->with(['images','store'])->where('status','active')->paginate(20);
+        // $category = Category::with('children:id,parent_id')->findOrFail($categoryId);
 
-        $ids = collect([$category->id])
-            ->merge($category->children->pluck('id'))
-            ->unique()->values();
-
-        return Product::with(['images','store'])
-            ->whereIn('category_id', $ids)
-            ->where('status','active')
-            ->paginate(20);
+        // $ids = collect([$category->id])
     }
 }
