4273e5850c47035ecc6259c6494ab9f52215f86c
hafiz sameer <hmstech11@gmail.com>
2025-09-29 16:28:16 +0500

    hel

--- app/Models/SavedItem.php
+++ app/Models/SavedItem.php
@@ -6,8 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class SavedItem extends Model
 {
-      protected $fillable = ['user_id','product_id'];
+      protected $fillable = ['user_id','product_id','service_id','post_id'];
 
     public function user() { return $this->belongsTo(User::class); }
     public function product() { return $this->belongsTo(Product::class)->with('images'); }
+    public function service() { return $this->belongsTo(Service::class); }
+    public function post() { return $this->belongsTo(Post::class); }
 }
