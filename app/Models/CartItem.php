95bccf7cb62c222c1540af9ef7f12aced3a9a41d
hafiz sameer <hmstech11@gmail.com>
2025-09-29 13:55:25 +0500

    hel

--- app/Models/CartItem.php
+++ app/Models/CartItem.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class CartItem extends Model
 {
     //
-     protected $fillable = ['cart_id','store_id','product_id','variant_id','qty','unit_price','unit_discount_price'];
+     protected $fillable = ['cart_id','store_id','product_id','variant_id','qty','unit_price','unit_discount_price','discount'];
     public function cart(){ return $this->belongsTo(Cart::class); }
     public function store(){ return $this->belongsTo(Store::class); }
     public function product(){ return $this->belongsTo(Product::class); }
