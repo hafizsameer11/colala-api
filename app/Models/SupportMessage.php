2c35ccf12c6bc09b8539eac930d0a5ad60dc269b
hafiz sameer <hmstech11@gmail.com>
2025-09-29 20:14:47 +0500

    jel '

--- app/Models/SupportMessage.php
+++ app/Models/SupportMessage.php
@@ -8,6 +8,6 @@ class SupportMessage extends Model
 {
     protected $fillable = ['ticket_id','sender_id','message','attachment','is_read'];
 
-    public function ticket(){ return $this->belongsTo(SupportTicket::class); }
+    public function ticket(){ return $this->belongsTo(SupportTicket::class,'ticket_id'); }
     public function sender(){ return $this->belongsTo(User::class,'sender_id'); }
 }
