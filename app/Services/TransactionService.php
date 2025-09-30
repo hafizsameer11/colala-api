de297ffb9d29c761f7b7036c48ff1d7bb61d81eb
hafiz sameer <hmstech11@gmail.com>
2025-09-30 10:02:14 +0500

    hel

--- app/Services/TransactionService.php
+++ app/Services/TransactionService.php
@@ -3,6 +3,7 @@
 
 namespace App\Services;
 use App\Models\Transaction;
+use App\Models\Wallet;
 use Illuminate\Support\Str;
 use Exception;
 use Illuminate\Support\Facades\DB;
@@ -22,7 +23,10 @@ public function getForUser($userId)
         ->where('type', 'order_payment')
         ->orderBy('created_at', 'desc')
         ->get();
+        $wallet=Wallet::where('user_id',$userId)->first();
+
     return [
+        'balance'=>$wallet ? $wallet->shopping_balance : 0,
         'withdrawals' => $withrdrawals,
         'deposits' => $deposits,
         'orderPayments' => $orderPayments,
