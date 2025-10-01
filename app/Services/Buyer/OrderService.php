8bb4ae8e19bd889330c4351ac8a6074b8855c03a
hafiz sameer <hmstech11@gmail.com>
2025-10-01 20:45:16 +0500

    hel

--- app/Services/Buyer/OrderService.php
+++ app/Services/Buyer/OrderService.php
@@ -12,7 +12,7 @@ class OrderService {
 
     public function detailForUser(int $userId, Order $order) {
         abort_unless($order->user_id === $userId, 403);
-        return $order->load(['storeOrders.store','storeOrders.items.product.images','orderTracking']);
+        return $order->load(['storeOrders.store','storeOrders.items.product.images','items.variant','orderTracking']);
     }
 
     public function buyerConfirmDelivered(int $userId, StoreOrder $storeOrder) {
