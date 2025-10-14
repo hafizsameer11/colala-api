912e94b41102f6a94ca5fb86808b22a52b91adcb
hafiz sameer <hmstech11@gmail.com>
2025-10-14 18:26:30 +0500

    hel

--- app/Http/Resources/BoostProductResource.php
+++ app/Http/Resources/BoostProductResource.php
@@ -32,15 +32,20 @@ class BoostProductResource extends JsonResource
             'start_date'     => optional($this->start_date)->toDateString(),
             'created_at'     => $this->created_at->toIso8601String(),
             'product' => [
-            'id'    => $this->product->id ?? null,
-            'name'  => $this->product->name ?? null,
-            'images'=> $this->product && $this->product->images 
-                        ? $this->product->images->map(fn($img) => [
-                            'id'  => $img->id,
-                            'url' => asset('storage/'.$img->path) // adjust field name if different
-                          ])
-                        : [],
-        ],
+                'id'    => $this->product->id ?? null,
+                'name'  => $this->product->name ?? null,
+                'price' => $this->product->price ?? null,
+                'discount_price' => $this->product->discount_price ?? null,
+                'final_price' => isset($this->product->discount_price) && $this->product->discount_price > 0
+                    ? $this->product->discount_price
+                    : ($this->product->price ?? null),
+                'images'=> $this->product && $this->product->images 
+                    ? $this->product->images->map(fn($img) => [
+                        'id'  => $img->id,
+                        'url' => asset('storage/'.$img->path)
+                    ])
+                    : [],
+            ],
             
         ];
     }
