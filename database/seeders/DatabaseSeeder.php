e3eda49e2acdbb283f6000cc060a23e63f30e228
hafiz sameer <hmstech11@gmail.com>
2025-11-20 13:49:07 +0500

    added category seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -20,8 +20,9 @@ class DatabaseSeeder extends Seeder
         //     'email' => 'test@example.com',
         // ]);
         $this->call([
-            CategorySeeder::class,
-            BrandSeeder::class,
+            // CategorySeeder::class,
+            // BrandSeeder::class,
+            MissingCategorySeeder::class,
         ]);
     }
 }
