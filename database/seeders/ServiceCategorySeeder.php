3e29ec8bd095ab67a3852cb3a276837cf8558383
hafiz sameer <hmstech11@gmail.com>
2025-12-26 12:13:40 +0500

    latest categories

--- database/seeders/ServiceCategorySeeder.php
+++ database/seeders/ServiceCategorySeeder.php
@@ -10,7 +10,7 @@ class ServiceCategorySeeder extends Seeder
     public function run()
     {
         // Clear existing service categories
-        ServiceCategory::truncate();
+        // ServiceCategory::truncate();
 
         $services = $this->getServiceCategories();
         $this->processServiceCategories($services);
