39990720fbbca3b7164825d749512cd087a7bee5
hafiz sameer <hmstech11@gmail.com>
2025-12-26 23:58:56 +0500

    email fixed

--- resources/views/emails/otp.blade.php
+++ resources/views/emails/otp.blade.php
@@ -7,7 +7,7 @@
 
 <body>
     <h2>Hello,</h2>
-    <p>Your One-Time Password (OTP) for Colala Express is:<=/p>
+    <p>Your One-Time Password (OTP) for Colala Express is:</p>
     <h1 style="font-size: 24px; color: #28a745;">{{ $otp }}</h1>
     <p>Please enter this code within the next 5 minutes to verify your account.</p>
     <p>If you did not request this, please ignore this email.</p>
